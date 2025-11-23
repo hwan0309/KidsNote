afeabf00ca98eb69f502adfa8798142241dd3257
yoon <protmxk@naver.com>
2025-11-23 21:10:44 +0900

    list image section 스타일 추가

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -2,10 +2,11 @@ import { getEmotionImage } from "../util/get-emotion-image";
 import Button from "./Button";
 import "./DiaryItem.css";
 const DiaryItem = () => {
+  const emotionId = 4;
   return (
     <div className="DiaryItem">
-      <div className="img_section">
-        <img src={getEmotionImage(1)} />
+      <div className={`img_section img_section_${emotionId}`}>
+        <img src={getEmotionImage(emotionId)} />
       </div>
       <div className="info_section">
         <div className="created_date">{new Date().toLocaleDateString()}</div>
